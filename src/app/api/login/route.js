114a9cb7344ed09d6c13c582a2c6594110c3174b
Edgar Misael Osornio Cervantes <emosornio@geb.mx>
2026-01-31 17:25:00 -0600

    Dashboard estilo Gaming y corrección de Login con Bcrypt

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -8,13 +8,6 @@ export async function POST(req) {
     const inputUser = usuario.trim();
     const inputPass = password.trim();
 
-    // 1. GENERADOR TEMPORAL (Solo para ver el hash en la terminal)
-    const hashGenerado = bcrypt.hashSync(inputPass, 10);
-    console.log("--- GENERADOR DE SEGURIDAD ---");
-    console.log("Si quieres que esta clave funcione, copia este Hash:");
-    console.log(hashGenerado); 
-    console.log("------------------------------");
-
     const FINAL_USER = '$S3rv!c3-D3sk';
     const FINAL_HASH = '$2b$10$S6WO2tiTY1F3WAGFNqjsMuTD4kLprtfzsDYr8wnbppGZ6M01jr0W6';
 
